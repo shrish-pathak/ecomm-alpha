5174d8c75e6feadd02159978133b556e697054f8
unknown <www.shrish@gmail.com>
2022-12-16 19:48:29 +0530

    added addresses model

---
+++ models/addresses.go
@@ -0,0 +1,15 @@
+package model
+
+import "gorm.io/gorm"
+
+// Addresses struct
+type Addresses struct {
+	gorm.Model
+	UserID       string `gorm:"not null" json:"userID"`
+	Mobile_No    int    `gorm:"not null" json:"mobileNo"`
+	City         string `gorm:"not null" json:"city"`
+	State        string `gorm:"not null" json:"state"`
+	Country_Code int    `gorm:"not null" json:"countryCode"`
+	Country      string `gorm:"not null" json:"country"`
+	Address      string `gorm:"not null" json:"address"`
+}
