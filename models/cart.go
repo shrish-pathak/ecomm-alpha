44609e731ef26c1add4a11cdb1c0ad7d63ce27b3
unknown <www.shrish@gmail.com>
2022-12-16 19:52:02 +0530

    added cart model

---
+++ models/cart.go
@@ -0,0 +1,12 @@
+package model
+
+import "gorm.io/gorm"
+
+// Cart struct
+type Cart struct {
+	gorm.Model
+	Product   Product
+	ProductID uint `gorm:"not null" json:"productID"`
+	Buyer     Buyer
+	BuyerID   uint `gorm:"not null" json:"buyerID"`
+}
